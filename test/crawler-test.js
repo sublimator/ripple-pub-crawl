257ceed017d4b467d20e099d1d1cae57607c2017
Nicholas Dudfield <ndudfield@gmail.com>
2015-04-28 18:51:49 +0700

    Use lodash more forcefully

--- test/crawler-test.js
+++ test/crawler-test.js
@@ -2,13 +2,14 @@ var assert = require('assert');
 var _ = require('lodash');
 var fs = require('fs');
 var util = require('util');
-var Crawler = require('../src/lib/crawler').Crawler;
-var normalizePubKey = require('../src/lib/crawler').normalizePubKey;
+var crawler = require('../src/lib/crawler');
+
+var Crawler = crawler.Crawler;
+var normalizePubKey = crawler.normalizePubKey;
 
 function TestCrawler() {
   Crawler.apply(this, arguments);
-  var noop = function(){};
-  this.logger = {log: noop, error: noop};
+  this.logger = {log: _.noop, error: _.noop};
 }
 
 util.inherits(TestCrawler, Crawler);
@@ -30,16 +31,14 @@ describe('TestCrawler', function() {
   var allpubs = {};
 
   function eachActive(func) {
-    _.forOwn(responses, function(resp, ip) {
-      resp.overlay.active.forEach(function(a){
-        func(a);
-      });
+    _.forOwn(responses, function(resp) {
+      resp.overlay.active.forEach(func);
     });
   }
   eachActive(function(a) {
     if (a.public_key) {
       a.public_key = normalizePubKey(a.public_key);
-      allpubs[a.public_key] = true; 
+      allpubs[a.public_key] = true;
     };
   });
   eachActive(function(a) {
@@ -52,7 +51,7 @@ describe('TestCrawler', function() {
       a.ip = pub2ip[a.public_key];
     };
   });
-  
+
   var expectedPeers = Object.keys(allpubs).length;
 
   beforeEach('function', function(){
@@ -64,12 +63,10 @@ describe('TestCrawler', function() {
   ips.slice(0, 10).forEach(function(ip) {
     it('it works for ip: ' + ip, function(done) {
       crawler.once('done', function() {
-        var vals = _.map(_.groupBy(crawler.byPub, function(v, k) {
-          return v.hops;
-        }), function(v, k){
-          return v.length;
-        });
-        var peers = vals.reduce(function(a,b){ return a+b});
+        function add(a, b) { return a + b; }
+        // [peers_at_hop_n, peers_at_hop_n+1, ...]
+        var peers_at_n_hops = _.map(_.groupBy(crawler.byPub, 'hops'), 'length');
+        var peers = peers_at_n_hops.reduce(add);
         assert(peers == 131);
         assert(peers == expectedPeers);
         done();
